d2741a15e609b6396a130cd4a11f88e657616c43
Elakiya <80253096+elakiya03@users.noreply.github.com>
2023-02-20 18:10:10 +0530

    Create usingArraylist.java

---
+++ usingArraylist.java
@@ -0,0 +1,10 @@
+public class usingArralist{
+  public static void main(String[] args){
+    ArrayList<Integer> al = new ArrayList<Integer>();
+    
+    al.add(5);
+    al.add(6);
+    
+    System.out.println(al);
+  }
+}
