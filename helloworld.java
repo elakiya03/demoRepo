421f39b26f735ef5ec25792c474a6ed13c5f30df
Elakiya <kselakiya2001@gmail.com>
2023-02-20 17:59:33 +0530

    Commiting HTML form and stylesheet

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,8 @@
 public class helloworld{
     public static void main(String... args){
-        // for(int i = 0; i < args.length; i++) {
-        //     System.out.println("Argument is: "+args[i]);
-        // }
+        for(int i = 0; i < args.length; i++) {
+            System.out.println("Everything");
+        }
 
         System.out.println("Elai");
     }
