8b107986f6d13154927fe78374eb85da491becab
Elakiya <kselakiya2001@gmail.com>
2023-02-20 15:29:04 +0530

    java file

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld{
+    public static void main(String... args){
+        // for(int i = 0; i < args.length; i++) {
+        //     System.out.println("Argument is: "+args[i]);
+        // }
+
+        System.out.println("Elai");
+    }
+}
